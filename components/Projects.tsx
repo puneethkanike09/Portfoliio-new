7d9775a76333f8d50e8b9ad7f945bea70871f2ae
Puneeth K <reachout.puneeth@gmail.com>
2024-11-19 22:25:56 +0530

    added new project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,62 @@ const Projects = () => {
     <section id="project" className="max-w-container mx-auto lgl:px-20 py-24">
       <SectionTitle title="Some Things I have Built" titleNo="03" />
       {/* ============ project One Start here ================ */}
+      
       <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
+        <div className="flex flex-col xl:flex-row-reverse gap-6">
+          <a
+            className="w-full xl:w-1/2 h-auto relative group"
+            href="https://mern-job-sns6.onrender.com/"
+            target="_blank"
+          >
+            <div>
+              <Image
+                className="w-full h-full object-contain rounded"
+                src={jobJunction}
+                alt="cyberImg"
+              />
+              <div className="absolute w-full h-full bg-textGreen/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
+            </div>
+          </a>
+          <div className="w-full xl:w-1/2 flex flex-col gap-6 justify-between items-end text-right z-10">
+            <div>
+              <p className="font-titleFont text-textGreen text-sm tracking-wide">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold">Cure Connect ( Doctor Appointment Web App )</h3>
+            </div>
+            <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md xl:-mr-16">
+              Engineered a full-stack job portal website that allows users to browse job listings, search for jobs, apply for
+              positions, and provides an <span className="text-textGreen">employer dashboard</span> for managing job applications.{" "}
+             
+              the portal.
+            </p>
+            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+              <li>React</li>
+              <li>Tailwind</li>
+              <li>Shadcn UI</li>
+              <li>Redux</li>
+              <li>Cloudinary</li>
+              <li>Render</li>
+            </ul>
+            <div className="text-2xl flex gap-4">
+              <a
+                className="hover:text-textGreen duration-300"
+                href="https://github.com/PuneethKanike/job-portal"
+                target="_blank"
+              >
+                <TbBrandGithub />
+              </a>
+              <a
+                className="hover:text-textGreen duration-300"
+                href="https://mern-job-sns6.onrender.com/"
+                target="_blank"
+              >
+                <RxOpenInNewWindow />
+              </a>
+            </div>
+          </div>
+        </div>
         <div className="flex flex-col xl:flex-row gap-6">
           <a
             className="w-full xl:w-1/2 h-auto relative group"
@@ -121,6 +176,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
+        
         {/* ============ project Two End here ================== */}
         {/* ============ project Three Start here ============== */}
         <div className="flex flex-col xl:flex-row gap-6">
